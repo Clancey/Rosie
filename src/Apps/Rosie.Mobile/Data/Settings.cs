76ceb64e8f1ce3b01d9d8346deaebe419e5fb7ba
James Clancey <james.clancey@gmail.com>
2017-10-27 11:50:00 -0800

    Converted to .net core

--- src/Apps/Rosie.Mobile/Data/Settings.cs
+++ src/Apps/Rosie.Mobile/Data/Settings.cs
@@ -32,7 +32,7 @@ namespace Rosie
 
 		public static void SetString (string value, [CallerMemberName] string memberName = "")
 		{
-			AppSettings.AddOrUpdateValue<string> (memberName, value);
+			AppSettings.AddOrUpdateValue (memberName, value);
 		}
 		public static int GetInt (int defaultValue = 0, [CallerMemberName] string memberName = "")
 		{
@@ -41,7 +41,7 @@ namespace Rosie
 
 		public static void SetInt (int value, [CallerMemberName] string memberName = "")
 		{
-			AppSettings.AddOrUpdateValue<int> (memberName, value);
+			AppSettings.AddOrUpdateValue (memberName, value);
 		}
 
 		public static long GetLong (long defaultValue = 0, [CallerMemberName] string memberName = "")
@@ -51,7 +51,7 @@ namespace Rosie
 
 		public static void SetLong (long value, [CallerMemberName] string memberName = "")
 		{
-			AppSettings.AddOrUpdateValue<long> (memberName, value);
+			AppSettings.AddOrUpdateValue (memberName, value);
 		}
 
 		public static bool GetBool (bool defaultValue = false, [CallerMemberName] string memberName = "")
@@ -61,19 +61,10 @@ namespace Rosie
 
 		public static void SetBool (bool value, [CallerMemberName] string memberName = "")
 		{
-			AppSettings.AddOrUpdateValue<bool> (memberName, value);
+			AppSettings.AddOrUpdateValue (memberName, value);
 		}
 
 
-		static T Get<T> (T defaultValue, [CallerMemberName] string memberName = "")
-		{
-			return AppSettings.GetValueOrDefault (memberName, defaultValue);
-		}
-
-		static void Set<T> (T value, [CallerMemberName] string memberName = "")
-		{
-			AppSettings.AddOrUpdateValue<T> (memberName, value);
-		}
 		#endregion
 	}
 }
---
+++ src/Core/Rosie/Data/SqliteExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections;
+namespace SQLite
+{
+	public static class SqliteExtensions
+	{
+		public static Task<int> InsertAsync(this SQLiteAsyncConnection connection, object obj, Type objType)
+ 		{
+ 			return Task.Factory.StartNew(() => {
+				var conn = connection.GetConnection();
+ 				using (conn.Lock ()) {
+ 					return conn.Insert(obj, objType);
+ 				}
+ 			});
+ 		}
+
+		public static Task<int> InsertAllAsync(this SQLiteAsyncConnection connection, IEnumerable items, string extra)
+		{
+			return Task.Factory.StartNew(() => {
+				var conn = connection.GetConnection();
+				using (conn.Lock())
+				{
+					return conn.InsertAll(items, extra);
+				}
+			});
+		}
+
+		public static Task<int> InsertOrReplaceAllAsync(this SQLiteAsyncConnection connection, IEnumerable items)
+		{
+			return  connection.InsertAllAsync(items, "OR REPLACE");
+		}
+	}
+}
