5a7786e25180235dfc6ceadabcc906988e9e5188
James Clancey <james.clancey@gmail.com>
2017-11-15 12:33:19 -0500

    changed states to cammel state

--- src/Core/Rosie.Models/DeviceStateKeys.cs
+++ src/Core/Rosie.Models/DeviceStateKeys.cs
@@ -3,40 +3,40 @@ namespace Rosie
 {
 	public partial class DeviceState
 	{
-		public const string SwitchState = "Switch State";
-		public const string BatteryLevel = "Battery Level";
-		public const string Lock = "Lock";
-		public const string Motion = "Motion";
-		public const string Level = "Level";
-		public const string Energy = "Energy";
-		public const string Power = "Power";
-		public const string About = "About";
-		public const string NetworkAddress = "Network Address";
-		public const string Status = "Status";
-		public const string SerialNumber = "Serial Number";
-		public const string Info = "Info";
-		public const string IndicatorStatus = "Indicator Status";
-		public const string Water = "Water";
-		public const string Button = "Button";
-		public const string NumberOfButtons = "Number of Buttons";
-		public const string Hue = "Hue";
-		public const string Saturation = "Saturation";
-		public const string Color = "Color";
-		public const string ColorTemperature = "Color Temperature";
-		public const string Presence = "Presence";
-		public const string Illuminance = "Illuminance";
-		public const string Temperature = "Temperature";
-		public const string Humidity = "Humidity";
-		public const string Acceleration = "Acceleration";
-		public const string UltravioletIndex = "Ultraviolet Index";
-		public const string Tamper = "Tamper";
-		public const string NeedUpdate = "Needs Update";
-		public const string Smoke = "Smoke";
-		public const string CarbonMonoxide = "CarbonMonoxide";
-		public const string AlarmState = "Alarm State";
-		public const string Contact = "Contact";
-		public const string Door = "Door";
-		public const string RSSI = "RSSI";
+		public const string SwitchState = "switchState";
+		public const string BatteryLevel = "batteryLevel";
+		public const string Lock = "lock";
+		public const string Motion = "motion";
+		public const string Level = "level";
+		public const string Energy = "energy";
+		public const string Power = "power";
+		public const string About = "about";
+		public const string NetworkAddress = "networkAddress";
+		public const string Status = "status";
+		public const string SerialNumber = "serialNumber";
+		public const string Info = "info";
+		public const string IndicatorStatus = "indicatorStatus";
+		public const string Water = "water";
+		public const string Button = "button";
+		public const string NumberOfButtons = "numberofButtons";
+		public const string Hue = "hue";
+		public const string Saturation = "saturation";
+		public const string Color = "color";
+		public const string ColorTemperature = "colorTemperature";
+		public const string Presence = "presence";
+		public const string Illuminance = "illuminance";
+		public const string Temperature = "temperature";
+		public const string Humidity = "humidity";
+		public const string Acceleration = "acceleration";
+		public const string UltravioletIndex = "ultraviolet Index";
+		public const string Tamper = "tamper";
+		public const string NeedUpdate = "needsUpdate";
+		public const string Smoke = "smoke";
+		public const string CarbonMonoxide = "carbonMonoxide";
+		public const string AlarmState = "alarmState";
+		public const string Contact = "contact";
+		public const string Door = "door";
+		public const string RSSI = "rssi";
 
 	}
 }
