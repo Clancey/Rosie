3d9a90c332052be1ebf78c9a1355ce0ea35f1f11
James Clancey <james.clancey@gmail.com>
2018-07-23 22:12:10 -0800

    Added some built in commands

--- src/Core/Rosie.Models/DeviceCommandKey.cs
+++ src/Core/Rosie.Models/DeviceCommandKey.cs
@@ -3,6 +3,39 @@ namespace Rosie
 {
 	public static class DeviceCommandKey
 	{
-		
+		public const string On = "on";
+		public const string Off = "off";
+		public const string Level = "level";
+		public const string Toggle = "toggle";
+		public const string Hue = "hue";
+		public const string Color = "color";
+		public const string Saturation = "saturation";
+		public const string Open = "open";
+		public const string Close = "close";
+		public const string Speed = "speed";
+		public const string Take = "take";
+		public const string Stop = "stop";
+		public const string Start = "start";
+		public const string Lock = "lock";
+		public const string Unlock = "unlock";
+		public const string Play = "play";
+		public const string Pause = "pause";
+		public const string Rewind = "rewind";
+		public const string FastForward = "fastforward";
+		public const string Mute = "mute";
+		public const string Push = "push";
+		public const string Notify = "notify";
+		public const string SetPoint = "setpoint";
+		public const string Poll = "poll";
+		public const string Refresh = "refresh";
+		public const string Speak = "speak";
+		public const string Mode = "mode";
+		public const string Cancel = "cancel";
+		public const string Beep = "beep";
+		public const string ChannelUp = "channelUp";
+		public const string ChannelDown = "channelDown";
+		public const string CaptureClip = "captureClip";
+		public const string Preset = "preset";
+
 	}
 }
