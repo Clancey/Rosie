d502bbdf6f4703f733becc01e9cc90b8e75d1b63
James Clancey <james.clancey@gmail.com>
2018-07-27 11:15:35 -0800

    Fixed iOS and android apps

--- src/Apps/Rosie.Mobile/Helpers/Locations.cs
+++ src/Apps/Rosie.Mobile/Helpers/Locations.cs
@@ -32,23 +32,7 @@ namespace Rosie.Mobile
 
 
 
-#if __OSX__
-		//[System.Runtime.InteropServices.DllImport("/System/Library/Frameworks/Foundation.framework/Foundation")]
-		//public static extern IntPtr NSHomeDirectory();
-
-		//public static string ContainerDirectory {
-		//	get {
-		//		var val = ((Foundation.NSString)ObjCRuntime.Runtime.GetNSObject(NSHomeDirectory())).ToString ();
-
-		//		if(val.Contains("Container"))
-		//			return val;
-		//		return Path.Combine (val, "Library/Application Support/gMusic");
-		//	}
-		//}
-		public static string BaseDir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-#else
 		public static string BaseDir = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.Personal)).ToString();
-#endif
 
 
 		public static readonly string LibDir = Path.Combine(BaseDir, "Library/");
