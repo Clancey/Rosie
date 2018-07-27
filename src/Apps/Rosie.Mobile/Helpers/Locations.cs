45ce736e29b18ba4dc9069f7002efc1de4633d00
Rui Marinho <me@ruimarinho.net>
2018-07-27 18:35:32 +0100

    Update to netcore2.1, update frontend to run on linux

--- src/Apps/Rosie.Mobile/Helpers/Locations.cs
+++ src/Apps/Rosie.Mobile/Helpers/Locations.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace Rosie.Mobile
 {
@@ -30,30 +28,19 @@ namespace Rosie.Mobile
 #endif
 		}
 
-
-
 #if __OSX__
-		//[System.Runtime.InteropServices.DllImport("/System/Library/Frameworks/Foundation.framework/Foundation")]
-		//public static extern IntPtr NSHomeDirectory();
-
-		//public static string ContainerDirectory {
-		//	get {
-		//		var val = ((Foundation.NSString)ObjCRuntime.Runtime.GetNSObject(NSHomeDirectory())).ToString ();
-
-		//		if(val.Contains("Container"))
-		//			return val;
-		//		return Path.Combine (val, "Library/Application Support/gMusic");
-		//	}
-		//}
 		public static string BaseDir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+		public static readonly string DocumentsDir = Path.Combine(BaseDir, "Documents/");
+#elif __LINUX__
+		public static string BaseDir = "";
+		public static readonly string DocumentsDir = Path.Combine(BaseDir, "");
 #else
 		public static string BaseDir = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.Personal)).ToString();
+		public static readonly string DocumentsDir = Path.Combine(BaseDir, "Documents/");
 #endif
-
 
 		public static readonly string LibDir = Path.Combine(BaseDir, "Library/");
 		public static readonly string ImageCache = Path.Combine(LibDir, "Images");
 		public static readonly string TmpDownloadDir = Path.Combine(BaseDir, "tmp");
-		public static readonly string DocumentsDir = Path.Combine(BaseDir, "Documents/");
 	}
 }
