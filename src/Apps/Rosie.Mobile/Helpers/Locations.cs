b9e42b018c7544adc137f5214ac94f07335922a3
James Clancey <james.clancey@gmail.com>
2018-07-27 11:31:21 -0800

    Fixed BaseFolder

--- src/Apps/Rosie.Mobile/Helpers/Locations.cs
+++ src/Apps/Rosie.Mobile/Helpers/Locations.cs
@@ -12,29 +12,27 @@ namespace Rosie.Mobile
 			try
 			{
 				Directory.CreateDirectory(Locations.ImageCache);
+				Directory.CreateDirectory(Locations.DocumentsDir);
 			}
 			catch (Exception ex)
 			{
 
 			}
-
-#if __IOS__
-			try
-			{
-				Foundation.NSFileManager.SetSkipBackupAttribute(Locations.ImageCache, true);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex);
-			}
-#endif
 		}
-
-
-
-		public static string BaseDir = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.Personal)).ToString();
-
-
+		static string _baseDir;
+		public static string BaseDir
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(_baseDir))
+				{
+					_baseDir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+					if (_baseDir.EndsWith("Documents"))
+						_baseDir = Directory.GetParent(_baseDir).ToString();
+				}
+				return _baseDir;
+			}
+		}
 		public static readonly string LibDir = Path.Combine(BaseDir, "Library/");
 		public static readonly string ImageCache = Path.Combine(LibDir, "Images");
 		public static readonly string TmpDownloadDir = Path.Combine(BaseDir, "tmp");
