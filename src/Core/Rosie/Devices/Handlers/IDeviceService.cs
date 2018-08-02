951a3093939996a504ce6e8869c5c9610ae51a0e
Jose Medrano <jose.medrano@microsoft.com>
2018-08-02 19:50:10 +0200

    Moves handlers to IDeviceService

--- src/Core/Rosie/Devices/Handlers/IDeviceService.cs
+++ src/Core/Rosie/Devices/Handlers/IDeviceService.cs
@@ -4,6 +4,9 @@ namespace Rosie
 {
 	public interface IDeviceService
 	{
+		event EventHandler<DeviceState> CurrentStateUpdated;
+		event EventHandler<Device> DeviceAdded;
+
 		string ServiceIdentifier { get; }
 
 		Task<bool> HandleRequest (Device device, DeviceUpdate request);
